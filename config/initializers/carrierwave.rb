80cd9827b90b825c4e91c4188326e490f6140e98
SAORI KODERA <tibezu.dash@gmail.com>
2024-02-20 14:02:05 +0900

    アクセスキーとシークレットアクセスキーの書き方を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      access_key_id: <%= ENV["AWS_ACCESS_KEY_ID"] %>
-      secret_access_key: <%= ENV["AWS_SECRET_ACCESS_KEY"] %>
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1', # リージョン
       path_style: true
     }
